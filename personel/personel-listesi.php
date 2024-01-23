728b44aa3e315a031c4be62572adaf5fbc0d473f
Mehmet Akif Dalhan <112507694+akifdlhan@users.noreply.github.com>
2024-01-23 18:02:23 +0300

    Güncelleme

--- personel/personel-listesi.php
+++ personel/personel-listesi.php
@@ -7,69 +7,68 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-9">
-                    <div id="urun-listesi" class="card">
+                <div class="col-lg-12">
+                    <div id="personel-listesi" class="card">
                         <div class="card-body">
-                            <div id="dugmeler" style="margin:2px;"></div>
-                            <table id="urunstoklistesi" class="table table-bordered dt-responsive table-responsive">
-                                <thead>
-                                    <tr>
-                                        <th>Tc</th>
-                                        <th>Personel Ad</th>
-                                        <th>Personel Soyad</th>
-                                        <th>Telefon</th>
-                                        <th>Cinsiyet</th>
-                                        <th>Departman</th>
-                                        <th>Adres</th>
-                                        <th>İl</th>
-                                        <th>İlçe </th>
-                                        <th>Başlama Tarihi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                        $fimy_personel = $vt ->listele("fimy_personel","ORDER BY ID DESC");
-                                        if($fimy_personel!=null) foreach($fimy_personel as $personel){
+                            <div id="dugmeler" style="margin: 2px;"></div>
+                            <div class="table-responsive">
+                                <table id="personelListesi" class="table table-bordered dt-responsive">
+                                    <thead>
+                                        <tr>
+                                            <th>TC</th>
+                                            <th>Adı</th>
+                                            <th>Soyadı</th>
+                                            <th>Telefon</th>
+                                            <th>Cinsiyet</th>
+                                            <th>Departman</th>
+                                            <th>Adres</th>
+                                            <th>İl</th>
+                                            <th>İlçe</th>
+                                            <th>Başlama Tarihi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $fimy_personel = $vt->listele("fimy_personel", "ORDER BY ID DESC");
+                                        if ($fimy_personel != null) foreach ($fimy_personel as $personel) {
                                             echo '
                                             <tr>
-                                            <td class="text-center">
-                                            <a href="PersonelID='.$personel['ID'].'">
-                                            <i style="font-size:16px; color:darkblue;" class="ti-layout-tab"></i>
-                                            </a>
-                                            </td>
-                                            <td>'.$personel['ID'].'</td>
-                                            <td>'.$personel['Adi'].'</td>
-                                            <td>'.$personel['Soyadi'].'</td>
-                                            <td>'.$personel['Telefon'].'</td>
-                                            <td>'.$personel['Cinsiyet'].'</td>
-                                            <td>'.$personel['Departman'].'</td>
-                                            <td>'.$personel['Adres'].'</td>
-                                            <td>'.$personel['Il'].'</td>
-                                            <td>'.$personel['Ilce'].'</td>
-                                            <td>'.$personel['BaslamaTarihi'].'</td>
-                                            </rt>';
+                                            <td>' . $personel['TC'] . '</td>
+                                            <td>' . $personel['Adi'] . '</td>
+                                            <td>' . $personel['Soyadi'] . '</td>
+                                            <td>' . $personel['Telefon'] . '</td>
+                                            <td>' . $personel['Cinsiyet'] . '</td>
+                                            <td>' . $personel['Departman'] . '</td>
+                                            <td>' . $personel['Adres'] . '</td>
+                                            <td>' . $personel['Il'] . '</td>
+                                            <td>' . $personel['Ilce'] . '</td>
+                                            <td>' . $personel['BaslamaTarihi'] . '</td>
+                                            </tr>';
                                         }
-
-                                    ?>
-                                </tbody>
-                            </table>
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-                
             </div>
         </div>
     </div>
-</div> 
+</div>
 <script>
-    $(document).ready(function(){
-        var personellistesidiv = document.getElementById("personel-listesi");
-        var personelYukseklik = personellistesidiv.clientHeight;
-        var personelsayfasibutonlarDiv = document.getElementById("personelsayfasibutonlar");
-        personelsayfasibutonlarDiv.style.height = personelYukseklik+"px";
+    $(document).ready(function () {
+        var table = $('#personelListesi').DataTable({
+            lengthMenu: [[50, 100, 250], [50, 100, 250]],
+            order: [[0, 'asc']],
+            dom: '<"top"lf<"wrapper"B>>rtip',
+            buttons: ["copy", "excel", "pdf"],
+        });
+
+        $('#dugmeler').append($('.dt-buttons'));
     });
 </script>
 <!-- ============================================================== -->
-    <!-- End Page content -->
-    <!-- ============================================================== -->
-    <?php include("../src/footer-alt.php"); ?>
+<!-- End Page content -->
+<!-- ============================================================== -->
+<?php include("../src/footer-alt.php"); ?>
